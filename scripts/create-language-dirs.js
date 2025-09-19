d68d00df361a3e63709ead8a6b6f4bec507e27af
Václav Gabriel <vgabriel@purple-technology.com>
2025-09-19 13:13:49 +0200

    fix: fix routes

--- scripts/create-language-dirs.js
+++ scripts/create-language-dirs.js
@@ -2,10 +2,25 @@ const fs = require("fs");
 const path = require("path");
 
 const languages = ["cs", "en", "pl", "sk", "de"];
+const routes = [
+  "",
+  "webhosting-nolimit",
+  "webhosting-lowcost",
+  "vps",
+  "website",
+  "disk",
+  "mailhosting",
+  "renewal",
+];
 const buildDir = path.join(__dirname, "../build");
 
 // Create language directories and copy files
 languages.forEach((lang) => {
+  // Skip 'cs' as it's the default language and doesn't need a prefix
+  if (lang === "cs") {
+    return;
+  }
+
   const langDir = path.join(buildDir, lang);
 
   // Create language directory if it doesn't exist
@@ -18,6 +33,20 @@ languages.forEach((lang) => {
     path.join(buildDir, "index.html"),
     path.join(langDir, "index.html")
   );
+
+  // Create subdirectories for each route and copy index.html
+  routes.forEach((route) => {
+    if (route) {
+      const routeDir = path.join(langDir, route);
+      if (!fs.existsSync(routeDir)) {
+        fs.mkdirSync(routeDir, { recursive: true });
+      }
+      fs.copyFileSync(
+        path.join(buildDir, "index.html"),
+        path.join(routeDir, "index.html")
+      );
+    }
+  });
 });
 
-console.log("Language directories created successfully!");
+console.log("Language directories and routes created successfully!");
